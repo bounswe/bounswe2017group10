4076e35e1ddfd0ac377b17ecaa49aa02c828e38e
emir350z <emirhan350z@gmail.com>
2017-04-23 19:34:20 +0300

    Add cultural Heritage

---
+++ API Project/src/main/java/hello/CulturalHeritageItem.java
@@ -0,0 +1,33 @@
+package hello;
+import java.util.Date;
+
+public class CulturalHeritageItem {
+ 	private long id;
+ 	private User owner;
+	private String title;
+	private String description;
+	//private ArrayList<Comment> comments;
+	//private ArrayList<MediaItem> mediaItems;
+	private String continent;
+	private String city;
+	//private ArrayList<Tag> tags;
+	//private AdvancedDate startDate;
+	//private AdvancedDate endDate;
+	private Date createdTime;
+	private Date updatedTime;
+	
+
+  public CulturalHeritageItem(int id, User owner, String title, String description, String continent, String city, Date createdTime, Date updatedTime) {
+	this.id = id;
+	this.owner=owner;
+	this.title = title;
+	this.description = description;
+	this.continent = continent;
+	this.city=city;
+	this.createdTime = createdTime;
+	this.updatedTime = updatedTime;
+  }
+
+
+
+}
