0f421c9f2da03fbb275f2b1603f6ac7d6b7e3a4f
egem <egemahlec@gmail.com>
2017-05-08 00:56:06 +0300

    Add exception handling to GreetingController Unit Test

--- API Project/src/test/java/com/bounswe/controllers/GreetingControllerTests.java
+++ API Project/src/test/java/com/bounswe/controllers/GreetingControllerTests.java
@@ -2,7 +2,7 @@ package com.bounswe.controllers;
 import com.bounswe.models.Greeting;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class GreetingControllerTests {
@@ -10,18 +10,22 @@ public class GreetingControllerTests {
   @Test
   public void greeting_whenInvokedWithName_thenSaysHelloName() {
 
-    // Given
-    GreetingController greetingController = new GreetingController();
+    try
+    {
+      // Given
+      GreetingController greetingController = new GreetingController();
+      Greeting cGreeting = greetingController.greeting("Name");
+      String strGreetingContent = cGreeting.getContent();
+      long nGreetingID = cGreeting.getId();
 
-    Greeting cGreeting = greetingController.greeting("Name");
-
-    String strGreetingContent = cGreeting.getContent();
-    long nGreetingID = cGreeting.getId();
-
-    // Then
-    assertThat(strGreetingContent).isEqualTo("Hello, Name!");
-    assertThat(nGreetingID).isEqualTo(1);
-
+      // Then
+      assertThat(strGreetingContent).isEqualTo("Hello, Name!");
+      assertThat(nGreetingID).isEqualTo(1);
+    }
+    catch(Exception e)
+    {
+      assertTrue("Exception caught",false);
+    }
   }
 
 }
