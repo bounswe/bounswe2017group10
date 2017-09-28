cb16a18318d341d003a1118b2f5e548f1c81df43
emir350z <emirhan350z@gmail.com>
2017-09-28 22:51:51 +0300

    Add localhost to allowed hosts

--- Atlas/backend/atlas/models.py
+++ Atlas/backend/atlas/models.py
@@ -8,6 +8,8 @@ from django.db import models
 class User(models.Model):
     username= models.CharField(max_length=200)
     age = models.IntegerField(default=0)
+    def __str__(self):
+        return self.username
 
 
 class Item(models.Model):
