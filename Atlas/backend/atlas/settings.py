e34ebb3f27e0d3848df915a4f3ac90dd521a9a66
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-10-05 10:41:29 +0300

    Close debug mode (IMPORTANT)

--- Atlas/backend/atlas/settings.py
+++ Atlas/backend/atlas/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b!ydx6z#0z%59+*@%(sfj&fynlk&73eyh4$+58%*ft%kfm43-c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['174.129.64.109',
                  '0.0.0.0',
