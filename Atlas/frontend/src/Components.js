d19fee0859ebc87bd3852075e3da3ed786b6ee47
bounaykut <aykut___1995@hotmail.com>
2017-10-07 03:50:30 +0300

    added search component in login page.

--- Atlas/frontend/src/Components.js
+++ Atlas/frontend/src/Components.js
@@ -3,5 +3,6 @@ import AtlasNavbar from './components/AtlasNavbar'
 import AtlasLogin from './components/AtlasLogin'
 import Home from './components/Home'
 import AtlasSignup from './components/AtlasSignup'
+import LoginSearch from './components/LoginSearch'
 
 export { Home, AtlasNavbar, AtlasLogin, AtlasSignup };
