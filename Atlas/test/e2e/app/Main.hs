00e181c110eb0ed1deafa5b062d62eef0d95c415
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-10-21 12:11:44 +0300

    Finalise login e2e tests

--- Atlas/test/e2e/app/Main.hs
+++ Atlas/test/e2e/app/Main.hs
@@ -3,8 +3,11 @@ module Main where
 
 import           Test.Hspec.WebDriver
 import qualified Test.WebDriver.Capabilities as W
+import           Test.WebDriver.Commands
 import           Test.WebDriver.Commands.Wait
 import           Data.Functor
+import           Control.Concurrent
+import           Control.Monad.IO.Class
 
 allBrowsers :: [Capabilities]
 allBrowsers = [chromeCaps]
@@ -18,4 +21,10 @@ main = hspec $
         it "clicks login" $ runWD $ do
           e <- findElem $ ByXPath "//a[contains(text(), 'Login')]"
           click e
-          void $ waitUntil 10 $ findElem $ ByXPath "//h3[@text='Log-in']"
+          _ <- waitUntil 10 $ findElem $ ByXPath "//*[contains(text(), 'Log-in')]"
+          loginInput <- findElem $ ByXPath "//input[@name='username']"
+          passInput <- findElem $ ByXPath "//input[@name='password']"
+          sendKeys "yigitozkavci" loginInput
+          sendKeys "220513Yigit" passInput
+          click =<< waitUntil 10 (findElem (ByXPath "//button[contains(text(), 'Log-in')]"))
+          liftIO $ threadDelay 10000
