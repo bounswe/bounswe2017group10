2c5e5def5a5e7f8f978eacd0cab427cb70de6e47
Esref Ozdemir <esref.ozdemir27@gmail.com>
2017-10-24 18:33:09 +0300

    Get all topics currently available at once.

--- Atlas/android/app/src/main/java/com/bounswe2017/group10/atlas/home/FeedFragment.java
+++ Atlas/android/app/src/main/java/com/bounswe2017/group10/atlas/home/FeedFragment.java
@@ -10,80 +10,46 @@ import com.bounswe2017.group10.atlas.adapter.FeedListAdapter;
 import com.bounswe2017.group10.atlas.adapter.FeedRow;
 import com.bounswe2017.group10.atlas.httpbody.CultureItem;
 import com.bounswe2017.group10.atlas.remote.APIUtils;
+import com.bounswe2017.group10.atlas.response.OnGetAllItemsResponse;
 import com.bounswe2017.group10.atlas.util.Constants;
 import java.util.ArrayList;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-import static com.bounswe2017.group10.atlas.util.Utils.tokenToAuthString;
+import static com.bounswe2017.group10.atlas.util.Utils.getSharedPref;
 
 public class FeedFragment extends ListFragment {
 
     private final ArrayList<CultureItem> mItemList = new ArrayList<>();
     private final ArrayList<FeedRow> mRowList = new ArrayList<>();
     private FeedListAdapter mAdapter;
-    private String authStr;
 
     @Nullable
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        authStr = getActivity().getIntent().getStringExtra(Constants.AUTH_STR);
-
-        // TODO: get all items
-        // TODO: get items only when created; not always.
-        APIUtils.serverAPI().getItem(authStr, 3).enqueue(new OnGetItemResponse());
-        APIUtils.serverAPI().getItem(authStr, 4).enqueue(new OnGetItemResponse());
-        APIUtils.serverAPI().getItem(authStr, 5).enqueue(new OnGetItemResponse());
+        String authStr = getSharedPref(getActivity()).getString(Constants.AUTH_STR, Constants.NO_AUTH_STR);
 
         mAdapter = new FeedListAdapter(getActivity(), mRowList);
         setListAdapter(mAdapter);
+
+        // currently, get all items
+        OnGetAllItemsResponse respHandler = new OnGetAllItemsResponse(getActivity(), mItemList, mRowList, mAdapter);
+        APIUtils.serverAPI().getAllItems(authStr).enqueue(respHandler);
     }
 
     @Override
     public void onListItemClick(ListView listView, View view, int pos, long id) {
-
+        // put item to bundle
+        Bundle itemBundle = new Bundle();
+        itemBundle.putParcelable(Constants.CULTURE_ITEM, mItemList.get(pos));
+        // put bundle to fragment
         ViewItemFragment viewItemFragment = new ViewItemFragment();
-        Bundle bundle = new Bundle();
-        bundle.putString("authStr", authStr);
-        viewItemFragment.setObj(mItemList.get(pos));
-        viewItemFragment.setArguments(bundle);
+        viewItemFragment.setArguments(itemBundle);
+        // go to fragment
         getActivity().getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.home_container, viewItemFragment)
                 .addToBackStack(null)
                 .commit();
-    }
-
-    /**
-     * Adds a culture item to current buffers and notifies the adapter of the changes.
-     *
-     * Culture item itself is added to culture items list. Its corresponding FeedRow object
-     * is added to FeedRow list.
-     *
-     * @param item CultureItem object to be stored.
-     */
-    private void addCultureItem(CultureItem item) {
-        mItemList.add(item);
-        mRowList.add(item.toFeedRow());
-        mAdapter.notifyDataSetChanged();
-    }
-
-    private class OnGetItemResponse implements Callback<CultureItem> {
-        @Override
-        public void onResponse(Call<CultureItem> call, Response<CultureItem> response) {
-            if (response.isSuccessful()) {
-                addCultureItem(response.body());
-            } else {
-                // TODO: Error checking
-            }
-        }
-
-        @Override
-        public void onFailure(Call<CultureItem> call, Throwable t) {
-            // TODO: Check network connections
-        }
     }
 }
 
