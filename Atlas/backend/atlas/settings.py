d617e9fce056edd6d31c0cc0a530ed2537eb8c5c
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-11-09 17:08:29 +0300

    Remove Turkish characters from settings.py

--- Atlas/backend/atlas/settings.py
+++ Atlas/backend/atlas/settings.py
@@ -184,7 +184,7 @@ JWT_AUTH = {
     'JWT_AUTH_HEADER_PREFIX': 'JWT',
 }
 
-ADMINS = [('Yiğit Özkavcı', 'yigitozkavci8@gmail.com'),
-          ('Eşref Özdemir', 'esref.ozdemir27@gmail.com'),
-          ('Talha Nişancı', 's.talhanisanci@gmail.com'),
+ADMINS = [('Yigit Ozkavci', 'yigitozkavci8@gmail.com'),
+          ('Esref Ozdemir', 'esref.ozdemir27@gmail.com'),
+          ('Talha Nisanci', 's.talhanisanci@gmail.com'),
           ('Aykut Bozkurt', 'aykut___1995@hotmail.com')]
