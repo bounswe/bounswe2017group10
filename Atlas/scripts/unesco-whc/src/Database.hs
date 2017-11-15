5d62c9c2641df417655bcdcdf0e4862102fa2f15
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-11-15 09:48:43 +0300

    Complete database part

--- Atlas/scripts/unesco-whc/src/Database.hs
+++ Atlas/scripts/unesco-whc/src/Database.hs
@@ -1,6 +1,24 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Database where
 
 import           Types
+import Database.PostgreSQL.Simple
+import Control.Arrow ((&&&))
+import Data.Time.Clock
+import qualified Data.Text as T
 
 insertHeritages :: [Heritage] -> IO ()
-insertHeritages _ = return ()
+insertHeritages (take 2 -> heritages) = do
+  now <- getCurrentTime
+  conn <- connectPostgreSQL "host='localhost' port=5432 dbname='atlas'"
+  executeMany conn
+    "INSERT INTO atlas_cultural_heritage(title, description, user_id, continent, country, city, public_accessibility, created_time, updated_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+    (map (toTuple now) heritages)
+  return ()
+    where
+      toTuple :: UTCTime -> Heritage -> (T.Text, T.Text, Int, String, String, String, Bool, UTCTime, UTCTime)
+      toTuple now Heritage {..} =
+        (_site, _shortDesc, 1, "", "", "", True, now, now) -- 1 is the user id of [Yigit Ozkavci]
