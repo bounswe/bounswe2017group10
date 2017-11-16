e8d28d67a7660cccc1b070ccd3d7feaecf94e8f4
Talha Nisanci <s.talhanisanci@gmail.com>
2017-11-16 18:29:11 +0200

    add endpoint to get all tags

--- Atlas/backend/atlas/tests.py
+++ Atlas/backend/atlas/tests.py
@@ -18,6 +18,7 @@ class cultural_heritage_item(TestCase):
         self.login_url = '/api/auth/login/'
         self.sigun_url = '/api/auth/signup/'
         self.cultural_heritage_item_url = '/cultural_heritage_item/'
+        self.tags_url = '/tags/'
         self.my_items_url= '/cultural_heritage_item/myitems'
         self.user = Account.objects.create_user(
             email=self.email, password=self.password, username=self.username)
@@ -568,3 +569,51 @@ class cultural_heritage_item(TestCase):
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response_content['tags']),0)
+    def test_get_tags(self):
+        item_data = {
+            "title": "Space needle",
+            'tags': [
+                {'name': 'place1',},
+                {'name': 'Seattle1'},
+                {'name': 'space1'},
+                {'name': 'Needle1'},
+                {'name': 'downtown1'}
+            ]
+        }
+        item_data2 = {
+            "title": "Discovery park",
+            'tags': [
+                {'name': 'place',},
+                {'name': 'Seattle'},
+                {'name': 'space'},
+                {'name': 'Needle'},
+                {'name': 'downtown'}
+            ]
+        }
+        response = self.client.post(
+            self.cultural_heritage_item_url,
+            item_data,
+            format='json',
+        )
+        self.assertEqual(response.status_code, 201)
+
+        response = self.client.post(
+            self.cultural_heritage_item_url,
+            item_data2,
+            format='json',
+        )
+        self.assertEqual(response.status_code, 201)
+        response = self.client.get(
+            self.tags_url,
+            format='json',
+        )
+        #test endpoint flexibility
+        response_content = json.loads(smart_text(response.content))
+        self.assertEqual(len(response_content),10)
+        response = self.client.get(
+            '/tags',
+            format='json',
+        )
+
+        response_content = json.loads(smart_text(response.content))
+        self.assertEqual(len(response_content), 10)
