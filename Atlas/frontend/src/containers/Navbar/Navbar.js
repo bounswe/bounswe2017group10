8929c66e27e1b3877e66bebdd44b39003c143bd0
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-11-23 10:22:53 +0300

    Implement frontend of the search

--- Atlas/frontend/src/containers/Navbar/Navbar.js
+++ Atlas/frontend/src/containers/Navbar/Navbar.js
@@ -2,18 +2,19 @@ import { connect } from 'react-redux';
 import Navbar from '../../components/Navbar/Navbar';
 import logo from '../../assets/images/logo.png';
 import { logout } from '../../actions/auth';
-import { toggleClosed , toggleOpen } from '../../actions/navbar';
-import { isLoggedIn } from '../../utils';
+import { toggleClosed , toggleOpen, updateSearchInput, selectSearchValue, updateSearchSuggestions } from '../../actions/navbar';
+import { isLoggedIn, authGet } from '../../utils';
+import { API_URL } from '../../constants.js';
 
 const mapStateToProps = state => {
   return {
+    token: state.auth.token,
     logo,
-      user: state.auth.user,
+    user: state.auth.user,
+    searchSuggestions: state.navbar.searchSuggestions,
     loggedIn: isLoggedIn(state.auth.user),
-      dropDownOpen : state.navbar.dropdownOpen,
-      amcam : state.navbar.drop
-
-
+    dropDownOpen: state.navbar.dropdownOpen,
+    searchInput: state.navbar.searchInput
   };
 }
 
@@ -26,9 +27,26 @@ const mapDispatchToProps = dispatch => {
     closeDrop: () => {
       dispatch(toggleClosed());
     },
-      openDrop: () => {
+    openDrop: () => {
       dispatch(toggleOpen());
-      }
+    },
+    updateSearchInput: (token, val) => {
+      dispatch(updateSearchInput(val));
+      (val === '')
+        ? dispatch(updateSearchSuggestions([]))
+        :
+          authGet(token, {
+            url: API_URL + '/cultural_heritage_item/search_autocorrect/' + val
+          }).then(resp =>
+            dispatch(updateSearchSuggestions(resp.data.results))
+          ).catch(err =>
+            console.log("Error on autocomplete: " + err)
+          );
+    },
+    selectSearchValue: (chId) => {
+      window.location = "/cultural-heritages/" + chId;
+      // dispatch(selectSearchValue(val));
+    }
   }
 }
 
