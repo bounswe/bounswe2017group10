4fd7fbcbf1c8db7b366ec3dd1943c2c83e944183
emir350z <emirhan350z@gmail.com>
2017-12-03 20:09:36 +0300

    Add react-moment for date passing

--- Atlas/frontend/src/components/CulturalHeritage/Comment.js
+++ Atlas/frontend/src/components/CulturalHeritage/Comment.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ClockIcon from 'react-icons/lib/fa/clock-o';
+import Moment from 'react-moment'
 
 const Comment = ({ comment }) => (
   <div>
     {comment.text}
     <hr />
     <span className="date">
-      <ClockIcon /> { comment.created_time }
+        <ClockIcon /> <Moment fromNow parse="YYYY-MM-DDTHH:mm:ssZ">{ comment.created_time } </Moment>
     </span>
   </div>
 )
