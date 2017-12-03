2ce4bf4841c0a84ae18b7f81c3adcae2213c7d09
emir350z <emirhan350z@gmail.com>
2017-12-03 23:21:56 +0300

    Add celery module and define task for tag extraction

---
+++ Atlas/backend/atlas/tasks.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, unicode_literals
+from .celery import app
+from .util import hidden_tag_extractor
+from rest_framework import serializers
+from rest_framework.utils import  model_meta
+from .models import Cultural_Heritage,hidden_tag
+
+
+class hidden_tag_serializer(serializers.ModelSerializer):
+    class Meta:
+        model =  hidden_tag
+        fields = '__all__'
+
+@app.task
+def add(x, y):
+    print("naber")
+    return x + y
+
+@app.task
+def extract_hidden_tags(item_id):
+    item= Cultural_Heritage.objects.get(pk=item_id)
+    description = item.description
+    hidden_tags = hidden_tag_extractor.extract_keywords(hidden_tag_extractor, text=description)
+    for tag in hidden_tags:
+        new_tag, created = hidden_tag.objects.get_or_create(name=tag)
+        if created:
+            item.hidden_tags.add(new_tag)
+    item.save()
