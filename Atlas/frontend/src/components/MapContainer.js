d255a690b2b295598ebd258506a565bfea9a066e
emir350z <emirhan350z@gmail.com>
2017-12-04 21:46:02 +0300

    Change map format and props

--- Atlas/frontend/src/components/MapContainer.js
+++ Atlas/frontend/src/components/MapContainer.js
@@ -2,14 +2,50 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import React from 'react';
 
 export class MapContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        }
+
+        // binding this to event-handler functions
+        this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.onMapClicked = this.onMapClicked.bind(this);
+    }
+
+    onMarkerClick(props, marker, e) {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            showingInfoWindow: true
+        });
+    }
+
+    onMapClicked(props) {
+        if (this.state.showingInfoWindow) {
+            this.setState({
+                showingInfoWindow: false,
+                activeMarker: null
+            })
+        }
+    }
     render() {
         return (
-            <Map  initialCenter={{
+            <Map  style = {{width: '400px', height:'400px'}} initialCenter={{
                 lat: 48,
-                lng: 2
+                lng: 40
             }} google={this.props.google} zoom={8}>
 
-                <Marker  name={this.props.locationName} position={{lat: this.props.lat, lng: this.props.lng}} />
+                <Marker onClick={this.onMarkerClick}
+                        name={'Current location'} />
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div>
+                        <h1>{this.state.selectedPlace.name}</h1>
+                    </div>
+                </InfoWindow>
 
             </Map>
         );
