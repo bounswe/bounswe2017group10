2099f38fcec67182a4a384683b66e58fdead8ec7
Esref Ozdemir <esref.ozdemir27@gmail.com>
2017-12-04 21:04:59 +0300

    Write popularity_score and trending_score tests.

--- Atlas/backend/atlas/popularity.py
+++ Atlas/backend/atlas/popularity.py
@@ -23,14 +23,14 @@ def popularity_score(item):
     """
     # admiration
     item_visit_obj = item_visit.objects.filter(cultural_heritage_item=item)
-    view_sec = item_visit_obj[0].duration() if item_visit_obj else 0
+    view_sec = item_visit_obj[0].duration if item_visit_obj else 0
     num_comments = item.comment_set.count()
     num_favorites = item.favorited_amount
     admiration = COEFF_VIEW_SEC*view_sec + COEFF_NUM_COMMENTS*num_comments + COEFF_NUM_FAVORITES*num_favorites
     # completeness
     blob = TextBlob(item.description)
-    description_complete = len(blob.noun_phrases) > COMPLETE_DESCRIPTION_NOUN_PHRASES
-    tags_complete = item.tags.count() > COMPLETE_TAG_NUM
+    description_complete = len(blob.noun_phrases) >= COMPLETE_DESCRIPTION_NOUN_PHRASES
+    tags_complete = item.tags.count() >= COMPLETE_TAG_NUM
     imgs_complete = item.image_media_item_set.count()
     location_complete = item.longitude is not None
     completeness = description_complete + tags_complete + imgs_complete + location_complete
@@ -58,7 +58,7 @@ def trending_score(item):
     popularity = popularity_score(item)
     # time passed
     creation_time = datetime.fromordinal(item.created_time.toordinal())
-    time_passed = (datetime.now() - creation_time).seconds/60
+    time_passed = (datetime.now() - creation_time).total_seconds()/60
     freshness = max(0, INITIAL_FRESHNESS - time_passed)
 
     return COEFF_POPULARITY*popularity + COEFF_FRESHNESS*freshness
