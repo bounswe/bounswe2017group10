780b62d7b34baa6adde214a85a0e8b0a22a308df
Esref Ozdemir <esref.ozdemir27@gmail.com>
2017-12-05 22:00:12 +0300

    Material design card view layout for ListItemsFragment.

--- Atlas/android/app/src/main/java/com/bounswe2017/group10/atlas/httpbody/UserResponse.java
+++ Atlas/android/app/src/main/java/com/bounswe2017/group10/atlas/httpbody/UserResponse.java
@@ -3,9 +3,10 @@ package com.bounswe2017.group10.atlas.httpbody;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class UserResponse {
+public class UserResponse implements Serializable {
 
     @SerializedName("firstname")
     @Expose
