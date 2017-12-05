f328a7806e5a4163afcb7bf9e2a58ca89a06818b
bounaykut <aykut___1995@hotmail.com>
2017-12-05 22:11:16 +0300

    initial map center was set

--- Atlas/frontend/src/components/MapContainer.js
+++ Atlas/frontend/src/components/MapContainer.js
@@ -5,9 +5,10 @@ export class MapContainer extends React.Component {
 
     render() {
         return (
-            <Map     onClick={this.props.mapClick} center={{
-                lat: this.props.lat,
-                lng: this.props.lng
+
+            <Map onClick={this.props.mapClick} center={{
+                lat: (typeof this.props.lat != 'undefined') ? this.props.lat:48,
+                lng: (typeof this.props.lng != 'undefined') ? this.props.lng:2
             }} google={this.props.google} zoom={8}  >
 
                 <Marker name={this.props.locationName} position={{"lat": this.props.lat, "lng": this.props.lng}} />
