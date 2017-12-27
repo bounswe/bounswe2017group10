cf09f57ef4c3ed2f2d2e95b2c128258de4b4e07a
Mutas <mmutas@gmail.com>
2017-12-27 21:32:06 +0300

    Profile editing feature on Android implemented.

---
+++ Atlas/android/app/src/main/java/com/bounswe2017/group10/atlas/httpbody/UpdateProfileRequest.java
@@ -0,0 +1,92 @@
+package com.bounswe2017.group10.atlas.httpbody;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class UpdateProfileRequest {
+
+    @SerializedName("email")
+    @Expose
+    private String email;
+
+    @SerializedName("old_password")
+    @Expose
+    private String oldPassword;
+
+    @SerializedName("password")
+    @Expose
+    private String password;
+
+    @SerializedName("confirm_password")
+    @Expose
+    private String confirmPassword;
+
+    @SerializedName("firstname")
+    @Expose
+    private String firstName;
+
+    @SerializedName("lastname")
+    @Expose
+    private String lastName;
+
+    @SerializedName("profile_picture")
+    @Expose
+    private String profilePicture;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(String profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+}
