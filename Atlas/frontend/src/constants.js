b461f5b6ec779be5154d30438b3094d04e7eaefd
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-12-28 10:30:07 +0300

    Fix ip addresses in frontend

--- Atlas/frontend/src/constants.js
+++ Atlas/frontend/src/constants.js
@@ -1,6 +1,7 @@
-// export const API_URL = 'http://174.129.53.155:81';
-export const API_URL = 'http://localhost:8000';
-export const ANNOTATION_SERVER_URL = 'http://localhost:8001';
+// Modify these in development environment
+export const SERVER_IP = 'http://174.129.53.155';
+export const BACKEND_PORT = '81';
+export const ANNOTATIONS_PORT = '82';
 
 export const TRUNCATE_LENGTH = 200;
 export const CULTURAL_HERITAGE_PAGINATION_LIMIT = 5;
@@ -10,3 +11,7 @@ export const ANNOT_IMG_SELECTOR_KEY = 'imageSelector';
 export const ANNOT_TXT_SELECTOR_KEY = 'textSelector';
 export const ANNOTATION_TXT_INPUT = 1
 export const ANNOTATION_IMG_INPUT = 2
+
+// Don't touch!
+export const API_URL = SERVER_IP + ':' + BACKEND_PORT;
+export const ANNOTATION_SERVER_URL = SERVER_IP + ':' + ANNOTATIONS_PORT;
